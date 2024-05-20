a130e75d6209d04c06d722024f990a3c48e8fb72
n0ffie <noffie.github@icloud.com>
2024-05-20 16:34:24 +0200

    update 0.0.1

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-echo "in the Bash"
+alias flog="ls -1 --color=never | fzf --preview "cat "| xargs tail -f "
+alias fman="compgen -c | fzf --preview "xargs man" | xargs man"
+
+alias cfunc="cat"
